61041ec2ab53cb73968b9e17ab5d06574ddae5af
Szymon <Szymon@DESKTOP-5QNL757>
2019-01-30 08:06:44 +0100

    final

---
+++ Assets/ballmove2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballmove2 : MonoBehaviour
+{
+    public bool isflat = true;
+     Rigidbody rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+
+        if(isflat)
+        {
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        }
+
+        rigid.AddForce(tilt*8);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.name =="end")
+        {
+            Destroy(gameObject,.2f);
+            Application.Quit();
+        }
+    }
+}
+
